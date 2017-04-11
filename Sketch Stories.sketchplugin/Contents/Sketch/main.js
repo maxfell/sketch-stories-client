31888cdd7cd0ae316eac56e553872ef99a95f3ba
Max Fellmuth <max@maxfellmuth.com>
2017-04-11 17:40:04 +0200

    feat: add uploading functionalities

--- Sketch Stories.sketchplugin/Contents/Sketch/main.js
+++ Sketch Stories.sketchplugin/Contents/Sketch/main.js
@@ -20,5 +20,34 @@ var shareSelectedSlice = function(context) {
   var image = NSImage.alloc().init().autorelease()
   image.addRepresentation(imageRep)
 
-  COSImageTools.viewNSImage_inWindowNamed_(image, "Share Slice")
+  //COSImageTools.viewNSImage_inWindowNamed_(image, "Share Slice")
+
+  var request = NSMutableURLRequest.alloc().init()
+  var url = NSURL.URLWithString("https://httpbin.org/post")
+  var boundary = "WebKitFormBoundary7MA4YWxkTrZu0gW"
+  var contentType = "multipart/form-data; boundary=" + boundary
+  
+  request.setURL(url)
+  request.setHTTPMethod("POST")
+  request.addValue_forHTTPHeaderField(contentType, "Content-Type")
+  
+  var body = NSMutableData.data()
+
+  body.appendData(NSString.stringWithFormat("\r\n--%@\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding))
+  body.appendData(NSString.stringWithFormat("Content-Disposition: form-data; name=\"%@\"; filename=\"photo.png\"\r\n", "storyImage").dataUsingEncoding(NSUTF8StringEncoding))
+  body.appendData(NSString.stringWithString("Content-Type: application/octet-stream\r\n\r\n").dataUsingEncoding(NSUTF8StringEncoding))
+  body.appendData(NSData.dataWithData(imageRep.representationUsingType_properties(NSPNGFileType, nil)))
+  body.appendData(NSString.stringWithFormat("\r\n--%@\r\n", boundary).dataUsingEncoding(NSUTF8StringEncoding))
+
+  request.setHTTPBody(body)
+
+  var error = NSError.alloc().init()
+  var responseCode = null
+
+  var oResponseData = NSURLConnection.sendSynchronousRequest_returningResponse_error_(request, responseCode, error)
+  var dataString = NSString.alloc().initWithData_encoding_(oResponseData, NSUTF8StringEncoding)
+
+  log("done")
+
+  log(dataString)
 }
